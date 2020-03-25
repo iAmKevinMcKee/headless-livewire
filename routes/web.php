97ef59712155d43547e3eaaee796469d8a0c838b
Kevin <kevin@intellow.com>
2020-03-25 09:02:27 -0500

    wip

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/videos/{video}', function () {
+    $video = App\Video::findOrFail(request()->video);
+    return view('video', ['video' => $video]);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 Auth::routes();
 
