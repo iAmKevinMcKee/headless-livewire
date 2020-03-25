36c36bd8f99a8b9846002d37191957a5ab9f053a
Kevin <kevin@intellow.com>
2020-03-25 10:02:09 -0500

    wip

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -4,8 +4,14 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-<div style="width: 100%; text-align: center; padding-top: 90px;">
-    Let's Interact With a Video
+<div style="width: 100%; text-align: center; padding-top: 90px; font-size: 20px;">
+    <h2>Let's Build a Screencasts Website</h2><br/>
+        <div>Track where the user is in the video</div>
+        <div>Record when the user completes the video</div>
+    <br/>
+    <br/>
+    <br/>
+    Video Model: {{$video}}
 </div>
 </body>
 </html>
